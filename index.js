d8c3139ea62a6b4df04f7060193368a2bb751547
nazwaaap <pradittanazwa@gmail.com>
2024-10-26 19:30:32 +0700

    Menambahkan script case command untuk index.js

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ const command = args[0];
 
 switch (command) {
     case "make-folder":
-        app.makeFolder()
+        app.makeFolder();
         break;
 
+    case "make-file":
+        app.makeFile();
+        break;
+
+    case "ext-sorter":
+        app.extSorter();
+        break;
+
+    case "read-folder":
+        app.readFolder();
+        break;
+
+    case "read-file":
+        app.readFile();
+        break;
+        
     default:
-        throw Error("Invalid command")
-        break;
+        throw Error("Invalid command");
 }
- 
-
